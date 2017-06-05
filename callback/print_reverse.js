badbfaac1d9ab178b734b98786c4d67681dcb3aa
chris <pachopa0320@gmail.com>
2017-06-05 20:32:05 +0000

    reverse all letters

---
+++ callback/print_reverse.js
@@ -0,0 +1,14 @@
+var getHTML = require('../http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/reverse.html'
+};
+
+function printUpperCase (html) {
+  
+  var result = html.split("").reverse().join('');
+  console.log(result);
+}
+
+getHTML(requestOptions, printUpperCase);
