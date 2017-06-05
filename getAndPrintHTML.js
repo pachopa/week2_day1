548df28ed14a4fef2901c707e565e2c395a5257b
chris <pachopa0320@gmail.com>
2017-06-05 19:22:25 +0000

    saved each chunks and print out at once

---
+++ getAndPrintHTML.js
@@ -0,0 +1,22 @@
+var https = require('https');
+
+
+
+function getAndPrintHTMLChunks () {
+  var chunks = '';
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+  https.get(requestOptions, function (response){
+  response.setEncoding('utf8');
+  response.on('data', function(data){
+    chunks += data;
+  });
+  response.on('end', function(){
+    console.log(chunks);
+  })
+  });
+}
+
+getAndPrintHTMLChunks();
